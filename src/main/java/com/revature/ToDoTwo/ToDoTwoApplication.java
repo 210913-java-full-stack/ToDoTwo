66986167a6fe4cce90eaf7e7e60db4f1cf909f40
Kyle <Kyle.Plummer@revature.com>
2021-11-01 12:44:52 -0400

    spring boot initializer boilerplate

---
+++ src/main/java/com/revature/ToDoTwo/ToDoTwoApplication.java
@@ -0,0 +1,13 @@
+package com.revature.ToDoTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/ToDoTwo/ToDoTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.ToDoTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
